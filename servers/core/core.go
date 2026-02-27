cb1784a6aef15359f9e1dcfaf001824db12128a9
PAWIU <70094237+PAW122@users.noreply.github.com>
2026-02-28 00:33:56 +0100

    Revert "v0.9.0 - init ui editor"
    This reverts commit c52cfe1caf2036556d71a023dd2003a6e845353d.

--- servers/core/core.go
+++ servers/core/core.go
@@ -2,31 +2,34 @@ package core
 
 /*
 	Core:
-		funkcje:
+		funckje:
 			hostowanie/uruchamianie serwera http
-				> lokalna obsluga db tak samo jak inne db
+				> lokalna obsługa db tak samo jak inne db
 			uruchamianie serwera do komunikacji z innymi serwerami db (ws / ptp)
-				> komunikacja pomiedzy serwerami github.com/PAW122/TsunamiDB
+				> komunikacja pomidzy serwerami github.com/PAW122/TsunamiDB
 			uruchamianie serwera do komunikacji z klientami (ws / ptp)
 				> komunikacja z klientami github.com/PAW122/TsunamiDB
 
-		moze w przyszlosci:
+
+		moze w przyszłośco:
 			auto updaty
-			obsluga wielu wersji db
+			obsługa wielu wersji db
 			custom ui
+
 */
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	config "github.com/PAW122/TsunamiDB/servers/config"
 	debug "github.com/PAW122/TsunamiDB/servers/debug"
 	networkmanager "github.com/PAW122/TsunamiDB/servers/network-manager"
 	public_api_v1 "github.com/PAW122/TsunamiDB/servers/public-api/v1"
 	subServer "github.com/PAW122/TsunamiDB/servers/subscriptions"
-	ui "github.com/PAW122/TsunamiDB/servers/ui"
 )
 
 var defaultConfigDir = "./config.json"
@@ -36,31 +39,34 @@ func RunCore() {
 	config.LoadConfig(defaultConfigDir)
 
 	debug.Log("Run Core")
+	config := flag.Bool("config", false, "load config from config.json")
+	flag.Parse()
 
-	opts, err := parseRunOptions(os.Args[1:])
-	if err != nil {
-		log.Fatalf("Blad argumentow: %v\n%s", err, usageMessage)
-	}
-
-	if opts.loadConfig {
+	if *config {
 		fmt.Println("load config")
 	}
 
-	fmt.Println("Starting network manager on port:", opts.corePort)
-
-	if opts.uiPort > 0 {
-		go func() {
-			if err := ui.RunAdminUI(opts.uiPort); err != nil {
-				log.Printf("UI server stopped: %v", err)
-			}
-		}()
+	if len(os.Args) < 2 {
+		log.Fatal("Użycie: go run main.go <port> [peer1] [peer2] ...")
 	}
 
-	networkmanager.StartNetworkManager(opts.corePort, opts.peers)
+	port, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal("Niepoprawny port:", err)
+	}
+	fmt.Println("Starting network manager on port: ", port)
+
+	// Lista znanych peerów (opcjonalna)
+	var knownPeers []string
+	if len(os.Args) > 2 {
+		knownPeers = os.Args[2:]
+	}
+
+	networkmanager.StartNetworkManager(port, knownPeers)
 
 	fmt.Println("Starting sub Sever on port:", 5845)
 	go subServer.StartWSServer("5845")
 
-	fmt.Println("Starting server on port:", 5844)
+	fmt.Println("Starting server on port: ", 5844)
 	public_api_v1.RunPublicApi_v1(5844)
 }
